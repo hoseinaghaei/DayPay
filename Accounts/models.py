7aa354262be477189f7334028b5a4e8b02e0c2ae
Behrad <behrad.fathi@igikala.com>
2023-08-15 07:58:13 +0330

    feat: define company model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(
+        max_length=150, unique=True, error_messages={
+            "unique": "A user with that username already exists.",
+        })
+    logo = models.ImageField()
+    max_credit_limit = models.PositiveIntegerField()
+
+
