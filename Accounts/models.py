4217ec0039efcf25dd3fe113453339d8f1ee2c8b
Behrad <behrad.fathi@igikala.com>
2023-08-15 08:40:43 +0330

    feat: define Employee model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .Enums import EmployeeEnums
+
 
 class Company(models.Model):
     name = models.CharField(
@@ -8,4 +11,22 @@ class Company(models.Model):
     logo = models.ImageField()
     max_credit_limit = models.PositiveIntegerField()
 
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    role = models.IntegerField(choices=EmployeeEnums.Roles, default=EmployeeEnums.Roles.TEAM_MEMBER)
+    national_id = models.CharField(max_length=10)
+    birth_date = models.DateField()
+    salary = models.PositiveBigIntegerField()
+    credit_rate_limit = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    gender = models.IntegerField(choices=EmployeeEnums.Genders)
+    marital_status = models.IntegerField(choices=EmployeeEnums.MaritalStatus)
+    credit_cart_number = models.CharField(max_length=16)
+    account_number = models.CharField(max_length=50)
+    sheba_number = models.CharField(max_length=50)
+    image = models.ImageField()
+    status = models.IntegerField(choices=EmployeeEnums.Status)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='employees')
 
