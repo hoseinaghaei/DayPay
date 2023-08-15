eb1a2e152b55026e7b2abee39c20039317e59860
Behrad <behrad.fathi@igikala.com>
2023-08-15 09:23:50 +0330

    feat: define User model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,6 +1,47 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import PermissionsMixin
+
 from .Enums import EmployeeEnums
+from .managers import UserManager
+from .validators import PhoneNumberValidator
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+    phone_number_validator = PhoneNumberValidator()
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField(blank=True)
+    phone_number = models.CharField(max_length=12, unique=True, validators=[phone_number_validator])
+    is_staff = models.BooleanField(
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(default=True)
+
+    object = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["phone_number"]
+
+    @classmethod
+    def normalize_phone_number(cls, phone_number):
+        """
+            remove 0 and add country code at the beginning off phone number
+        """
+        return phone_number
 
 
 class Company(models.Model):
@@ -10,6 +51,8 @@ class Company(models.Model):
         })
     logo = models.ImageField()
     max_credit_limit = models.PositiveIntegerField()
+
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
 
 
 class Employee(models.Model):
@@ -28,5 +71,6 @@ class Employee(models.Model):
     image = models.ImageField()
     status = models.IntegerField(choices=EmployeeEnums.Status)
 
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='employees')
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='employees')
 
