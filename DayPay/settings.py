215c912fffac35ea967f5287b9db19852eca5a0b
Behrad <behrad.fathi@igikala.com>
2023-08-16 07:00:57 +0330

    feat: set Media root and url

--- DayPay/settings.py
+++ DayPay/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files
+
+MEDIA_ROOT = BASE_DIR / 'Media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
