79ecc5e8ee99b9d0e4b107d90e853c62af04b15f
hoseinaghaei <h.aghaei.araei@gmail.com>
2023-08-20 22:17:01 +0330

    add view and serializer for treasury app.

--- Treasury/urls.py
+++ Treasury/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import GiftAPI
 
 urlpatterns = [
-
+    path('gift/add', GiftAPI.as_view()),
+    path('wallet/deposit', ),
+    path('wallet/withdraw', ),
+    path('wallet/show', ),
 ]
--- Treasury/views.py
+++ Treasury/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class GiftAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        pass
+
+
+class CreditAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class WalletAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
