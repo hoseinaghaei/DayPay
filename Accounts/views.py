ea96888a32c73edf8c893fd0a252e3f361618195
Behrad <behrad.fathi@igikala.com>
2023-08-23 12:57:28 +0330

    feat: add account APIs

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, generics, status
+
+
+class LoginApi(APIView):
+    def post(self, request):
+        pass
+
+
+class SendOTPApi(APIView):
+    def post(self, request):
+        pass
+
+
+class VerifyOTPApi(APIView):
+    def post(self, request):
+        pass
+
+
+class LogoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        pass
+
+
+class CompanyProfileApi(APIView):
+    def get(self, request):
+        pass
+
+
+class EmployeeProfileApi(APIView):
+    def get(self, request):
+        pass
