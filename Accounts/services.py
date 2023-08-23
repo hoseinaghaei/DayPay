91970844d8f84d9a2e108d4d42e151a012c28c79
Behrad <behrad.fathi@igikala.com>
2023-08-23 15:30:38 +0330

    feat: add accounting service

---
+++ Accounts/services.py
@@ -0,0 +1,12 @@
+class AccountingService:
+    def generate_opt(self):
+        pass
+
+    def login_otp(self):
+        pass
+
+    def login_password(self):
+        pass
+
+    def logout(self):
+        pass
