512ecccfba89a5226d94a6d16ba5a048302a3b4a
Behrad <behrad.fathi@igikala.com>
2023-08-23 16:43:30 +0330

    feat: add login api

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 
-from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import permissions, generics, status
 
+from Bases.base_views import BaseAPIView
+from .serializers import LoginSerializer
+from .services import AccountingService
 
-class LoginApi(APIView):
-    def post(self, request):
-        pass
 
+class LoginApi(BaseAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request: Request):
+        try:
+            validated_data = self._prepare_validated_data(request)
+            response = AccountingService.login_password(validated_data)
+            return response
+        except Exception as e:
+            return Response(data={"error": e}, status=status.HTTP_400_BAD_REQUEST)
 
 class SendOTPApi(APIView):
     def post(self, request):
