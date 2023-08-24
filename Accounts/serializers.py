4c65ce366677384261412a2ef212ecb2433c3d86
Behrad <behrad.fathi@igikala.com>
2023-08-24 18:23:59 +0330

    feat: use BaseSerializer

--- Accounts/serializers.py
+++ Accounts/serializers.py
@@ -1,14 +1,10 @@
 from rest_framework import serializers
 
+from Bases.base_serializers import BaseSerializer
 
-class LoginSerializer(serializers.Serializer):
+
+class LoginSerializer(BaseSerializer):
     phone_number = serializers.CharField(max_length=12)
     username = serializers.CharField(max_length=150, required=False)
     password = serializers.CharField(max_length=128, required=False)
     otp = serializers.CharField(max_length=5, required=False)
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
