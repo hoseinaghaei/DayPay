1d797866b13b8b268bb5666764bc72a9ee77da7f
Behrad <behrad.fathi@igikala.com>
2023-08-24 17:01:55 +0330

    feat: implement login with password

--- Accounts/services.py
+++ Accounts/services.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import authenticate
+from django.apps import apps
+
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import Employee, Company
+
+
 class AccountingService:
     def generate_opt(self):
         pass
@@ -5,8 +15,22 @@ class AccountingService:
     def login_otp(self):
         pass
 
-    def login_password(self):
-        pass
+    @staticmethod
+    def login_password(data: dict, account_class: str) -> Response:
+        user = authenticate(phone_number=data["phone_number"], password=data["password"])
+        if user and user.is_active:
+            account = apps.get_model("Accounts", account_class).objects.get(user=user)
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            return Response({
+                'access_token': access_token,
+                'refresh_token': refresh_token,
+                'account_id': account.id
+            })
+
+        raise serializers.ValidationError("Incorrect credentials.")
 
     def logout(self):
         pass
