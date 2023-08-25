651e3573ffe46a557a530500fdb1926cc38d67ce
Behrad <behrad.fathi@igikala.com>
2023-08-25 22:16:34 +0330

    fix: fix getting credit

--- Treasury/services.py
+++ Treasury/services.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status
 from django.db import transaction
-from django.db.models import F
+from django.db.models import F, Sum
+from django.db.models.functions import Coalesce
 
 from .models import Gift, Employee, Wallet, WalletTransaction, Company, Transaction
 from .Enum import WalletTransactionEnums
@@ -155,11 +156,11 @@ class TreasuryService:
     def get_taken_credit_in_current_month(employee: Employee):
         first_day_of_month = jalali_first_day_of_month()
         return WalletTransaction.objects.filter(
-            wallet=employee.wallet,
+            wallet=employee.wallet.get(active=True),
             type=WalletTransactionEnums.Types.DEPOSIT,
             source=WalletTransactionEnums.Sources.EMPLOYEE,
             date__gte=first_day_of_month
-        ).aggregate(sum('amount')).get('amount__sum', 0)
+        ).aggregate(total_amount=Coalesce(Sum('amount'), 0)).get("total_amount")
 
     @staticmethod
     def get_company_remaining_credit(company_id: int):
