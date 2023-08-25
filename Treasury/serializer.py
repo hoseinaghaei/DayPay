859e7232c69327e5b02419352eda57909622204e
Behrad <behrad.fathi@igikala.com>
2023-08-25 17:35:03 +0330

    fix: update serializer classes

--- Treasury/serializer.py
+++ Treasury/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from Bases.base_serializers import BaseSerializer
 
-from .models import Wallet
+from .Enum import TransactionEnum
 
 
 class GiftSerializer(BaseSerializer):
@@ -16,37 +16,6 @@ class GiftSerializer(BaseSerializer):
         pass
 
 
-class WithdrawCreditSerializer(BaseSerializer):
-    amount = serializers.IntegerField()
-    wallet_id = serializers.IntegerField()
-    type = serializers.CharField(max_length=100)  # todo : ChoiceField
-
-    def validate_amount(self):
-        pass
-
-    def validate_wallet_id(self):
-        pass
-
-    def validate_type(self):
-        pass
-
-
-class GetCreditSerializer(BaseSerializer):
-    credit_amount = serializers.IntegerField()
-
-
-class GetWalletSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Wallet
-        fields = ('total_amount', 'gift_amount', 'credit_amount')
-
-
-class DepositCreditSerializer(BaseSerializer):
-    credit_amount = serializers.IntegerField()
-    wallet_id = serializers.IntegerField()
-
-    def validate_credit_amount(self):
-        pass
-
-    def validate_wallet_id(self):
-        pass
+class WalletSerializer(BaseSerializer):
+    employee_id = serializers.IntegerField()
+    type = serializers.ChoiceField(choices=TransactionEnum.Types.choices, required=False)
