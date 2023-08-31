ed21c2cffa78cda98536d7ef40a2f2a4951b76bc
Behrad <behrad.fathi@igikala.com>
2023-09-01 03:27:03 +0330

    feat: add new urls

--- Treasury/urls.py
+++ Treasury/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path('gift/add/', GiftAPI.as_view()),
     path('wallet/deposit/', DepositWalletApi.as_view()),
+    path('withdraw/detial/', GetWithdrawDetails.as_view()),
+    path('withdraw/otp/', GenerateWithdrawOTP.as_view()),
     path('wallet/withdraw/', WithdrawWalletAPI.as_view()),
     path('wallet/show/', GetWalletApi.as_view()),
     path('get/credit/', GetCreditApi.as_view())
