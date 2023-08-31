c1a5dd60227958c9ecd54a9a5048b0388758a768
Behrad <behrad.fathi@igikala.com>
2023-09-01 03:22:02 +0330

    feat: update gift model admin

--- Treasury/admin.py
+++ Treasury/admin.py
@@ -5,7 +5,15 @@ from .models import *
 
 @admin.register(Gift)
 class GiftAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['get_company', 'get_employee', 'amount']
+
+    @admin.display(description='Employee')
+    def get_employee(self, obj: Gift):
+        return obj.employee
+
+    @admin.display(description='Company')
+    def get_company(self, obj: Gift):
+        return obj.company
 
 
 @admin.register(Wallet)
