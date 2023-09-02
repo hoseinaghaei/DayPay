3b16387273d72d9f8eb1d03d5fcc839f503ec584
Behrad <behrad.fathi@igikala.com>
2023-09-02 13:20:10 +0330

    feat: update wallet transaction serializer

--- Reports/serializers.py
+++ Reports/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 
-from Treasury.models import WalletTransaction
+from Treasury.models import WalletTransaction, Transaction, Wallet
+from Accounts.serializers import EmployeeSerializer
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    employee = EmployeeSerializer()
+
+    class Meta:
+        model = Wallet
+        fields = ['employee']
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transaction
+        fields = ['transfer_mode', 'transfer_id', 'commission', 'status']
 
 
 class WalletTransactionSerializer(serializers.ModelSerializer):
+    transfer_id = TransactionSerializer(many=False, read_only=True)
+    wallet = WalletSerializer()
 
     class Meta:
         model = WalletTransaction
